f537168fc76c7e7e87a5c604ac54dbd2d75be710
AnnaUlyanova <anna.ulyanova@lightbox.co.nz>
2018-01-21 18:18:34 +1300

    move App login functionality to App container, add recompose

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { auth, provider } from './firebase';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 import MainComponent from './components/MainComponent';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: null,
-    };
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-  }
-
-  componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({ user });
+function App({
+  user,
+  login,
+  logout,
+}) {
+  return (
+    <div className="App">
+      {user ?
+        <button onClick={logout}>Log Out</button>
+       :
+        <button onClick={login}>Log In</button>
       }
-    });
-  }
-
-  login() {
-    auth.signInWithPopup(provider)
-      .then((result) => {
-        const user = result.user;
-        this.setState({
-          user,
-        });
-      });
-  }
-
-  logout() {
-    auth.signOut()
-      .then(() => {
-        this.setState({
-          user: null,
-        });
-      });
-  }
-
-  render() {
-    const { user } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {user ?
-          <button onClick={this.logout}>Log Out</button>
-         :
-          <button onClick={this.login}>Log In</button>
-        }
-        {user ?
-          <div>
-            <p>Welcome!</p>
-            <MainComponent />
-          </div>
-          :
-          <p className="App-intro">
-            Hi, please log in.
-          </p>
-        }
-      </div>
-    );
-  }
+      {user ?
+        <div>
+          <p>Welcome!</p>
+          <MainComponent />
+        </div>
+        :
+        <p className="App-intro">
+          Hi, please log in.
+        </p>
+      }
+    </div>
+  );
 }
+
+App.propTypes = {
+  user: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  login: PropTypes.func.isRequired,
+  logout: PropTypes.func.isRequired,
+};
+
+App.defaultProps = {
+  user: null,
+};
 
 export default App;
---
+++ src/containers/App.jsx
@@ -0,0 +1,39 @@
+import {
+  compose,
+  lifecycle,
+  withState,
+  withHandlers,
+} from 'recompose';
+import { auth, provider } from '../firebase';
+
+import App from '../App';
+
+const enhance = compose(
+  withState('user', 'setUser', null),
+  withHandlers({
+    login: ({ setUser }) => () => {
+      auth.signInWithPopup(provider)
+        .then((result) => {
+          const { user } = result;
+          setUser({ user });
+        });
+    },
+    logout: ({ setUser }) => () => {
+      auth.signOut()
+        .then(() => {
+          setUser(null);
+        });
+    },
+  }),
+  lifecycle({
+    componentDidMount() {
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          this.props.setUser({ user });
+        }
+      });
+    },
+  }),
+)
+
+export default enhance(App);
